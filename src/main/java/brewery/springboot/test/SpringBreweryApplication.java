2d59c60b7a95f84b11a7571a941147cf2c67edcf
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-21 14:26:16 -0700

    adds circleci

--- src/main/java/brewery/springboot/test/SpringBreweryApplication.java
+++ src/main/java/brewery/springboot/test/SpringBreweryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTestApplication {
+public class SpringBreweryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTestApplication.class, args);
+        SpringApplication.run(SpringBreweryApplication.class, args);
     }
 
 }
